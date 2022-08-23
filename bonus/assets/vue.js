a078c06475e700210046febd1a2c0d7e0321b0e8
M6Irinel <momoloacairinel666@gmail.com>
2022-08-23 15:42:40 +0200

    bonus carousel

---
+++ bonus/assets/vue.js
@@ -0,0 +1,44 @@
+const app = new Vue( {
+    el: '#vue',
+    data () {
+        return {
+            title: `Carousel`,
+            img_src: `https://picsum.photos/id/1/600/400`,
+            img_alt: 'Image from Lorem Picsum',
+            img_id: 1,
+            range_id_min: 30,
+            range_id_max: 40,
+            time_autoPlay: 4000,
+            interv: setInterval( this.img_next, this.time_autoPlay )
+        }
+    },
+    methods: {
+        start () {
+            this.img_id = this.range_id_min;
+        },
+
+        autoPlay () {
+            clearInterval( this.interv );
+            this.interv = setInterval( this.img_next, this.time_autoPlay );
+        },
+
+        img_next () {
+            this.autoPlay();
+
+            if ( this.img_id > this.range_id_max )
+                this.img_id = this.range_id_min;
+            
+            this.img_src = `https://picsum.photos/id/${this.img_id++}/600/400`;
+        },
+        
+        img_prev () {
+            this.autoPlay();
+
+            if ( this.img_id < 1 )
+                this.img_id = this.range_id_max;
+
+            this.img_src = `https://picsum.photos/id/${this.img_id--}/600/400`;
+        }
+    },
+} );
+app.start();
